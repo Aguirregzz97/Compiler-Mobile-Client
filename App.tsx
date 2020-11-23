0d5408cb8760eeb5ce9b253f6da1a5d5899d3e92
Andres <aguirregzz97@gmail.com>
2020-11-23 17:59:46 -0600

    Add base for api call

--- App.tsx
+++ App.tsx
@@ -1,23 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, SafeAreaView, TextInput, View, Button, Alert } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-
-  function handleButtonPress() {
-    Alert.alert('Button with adjusted color pressed')
-  }
 
   return (
     <SafeAreaView style={styles.container}>
       <Text style={ styles.heading }>Compilords!</Text>
       <CodeText />
-      <View style={ { marginTop: 20  } }>
-        <Button
-          title='RUN CODE!'
-          onPress={ handleButtonPress }
-        />
-      </View>
       <StatusBar style="inverted" />
     </SafeAreaView>
   )
@@ -25,29 +15,52 @@ export default function App() {
 
 
 const CodeText = () => {
-  const [value, onChangeText] = React.useState(`
+  const [codeText, onChangeCodeText] = React.useState(`
   Program program;
 
   main () {
   
   }`)
 
+  const [output] = React.useState('Output:\n')
+
+  async function handleButtonPress() {
+    const response = await fetch('http://localhost:5000/run', { method: 'GET', body: codeText })
+    console.log(response)
+  }
+
   return (
-    <TextInput
-      textAlignVertical='top'
-      multiline
-      numberOfLines={40}
-      style={ styles.inputStyle }
-      onChangeText={text => onChangeText(text)}
-      value={value}
-    />
+    <>
+      <TextInput
+        textAlignVertical='top'
+        multiline
+        numberOfLines={40}
+        style={ styles.inputStyle }
+        onChangeText={text => onChangeCodeText(text)}
+        value={codeText}
+      />
+      <TextInput
+        textAlignVertical='top'
+        multiline
+        numberOfLines={40}
+        style={ styles.outputStyle }
+        value={output}
+      />
+      <View style={ { marginTop: 20  } }>
+        <Button
+          title='RUN CODE!'
+          onPress={ handleButtonPress }
+        />
+      </View>
+    </>
   )
 }
 
 const styles = StyleSheet.create({
   heading: {
     fontSize: 25,
-    marginBottom: 35,
+    marginTop: 20,
+    marginBottom: 20,
     color: 'white',
   },
   white: {
@@ -61,8 +74,17 @@ const styles = StyleSheet.create({
   },
   inputStyle: {
     color: 'white',
-    width: '80%',
-    height: '60%',
+    width: '85%',
+    height: '40%',
+    borderColor: 'white',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  outputStyle: {
+    marginTop: 20,
+    color: 'white',
+    width: '85%',
+    height: '30%',
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 5,
